e6dcc39a23e5b221168b2407659246581c1365de
Heyme <heyme.coraspe@gmail.com>
2024-03-31 21:10:30 -0300

    formulario de registro listo, empezando JS

--- javascript.js/pagina-pacienta.js
+++ javascript.js/pagina-pacienta.js
@@ -1,11 +1,11 @@
 /*Turnos ya creados*/
 
 turnos= [
-    {fecha: 4, mes: "Abril", especialidad: "Cardiología", medico: "Juan Pérez"},
-    {fecha: 17, mes: "Abril", especialidad: "Dermatología", medico: "Yanina Durán",},
-    {fecha: 26, mes: "Abril", especialidad: "Ginecología", medico: "Luis Soto",},
-    {fecha: 6, mes: "Mayo", especialidad: "Alergias", medico: "Florencia Molina",},
-    {fecha: 10, mes: "Mayo", especialidad: "Fonoaudiología", medico: "Carmen Bueno",},
+    {fecha: 4, mes: "Abril", turno:"Mañana",especialidad: "Cardiología", medico: "Juan Pérez"},
+    {fecha: 17, mes: "Abril", turno:"Mañana", especialidad: "Dermatología", medico: "Yanina Durán",},
+    {fecha: 26, mes: "Abril", turno:"Tarde", especialidad: "Ginecología", medico: "Luis Soto",},
+    {fecha: 6, mes: "Mayo", turno:"Tarde", especialidad: "Alergias", medico: "Florencia Molina",},
+    {fecha: 10, mes: "Mayo", turno:"Tarde",especialidad: "Fonoaudiología", medico: "Carmen Bueno",},
 ];    
 
 //funciones
@@ -13,7 +13,7 @@ const misTurnosCards= document.getElementById("misTurnos");
 //elementos de la lista
 const cardsMisTurnos=() =>{
     turnos.forEach((element) => {
-        detalleTurnos= `${element.fecha},${element.mes} ${element.especialidad}, ${element.medico} `
+        detalleTurnos= `${element.fecha}, ${element.mes}, ${element.turno}, ${element.especialidad}, ${element.medico} `
         const nuevaCard= document.createElement("li");
         nuevaCard.classList.add("list-group-item");
         nuevaCard.innerHTML=detalleTurnos;
@@ -21,6 +21,24 @@ const cardsMisTurnos=() =>{
     });
 
 }
+
+//nuevo turno
+const get_Date= document.getElementById("inpunt-fecha");
+const get_Month= document.getElementById("inpunt-mes");
+const selectMorning= document.getElementById("input-TurnoMañana");
+const selectAfternoon= document.getElementById("input-TurnoTarde");
+const selectSpeciality= document.getElementById("especialidad-NvoTurno");
+const selectDoctor= document.getElementById("especialista-NvoTurno");
+
+
+const alertDate=()=>{
+    if (get_Date <0 && get_Date> 31){
+        
+    }
+}
+
+
+
 
 
 //crear calendrio
