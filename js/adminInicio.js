3b71c1c188799f1dbea9b254259cb1d36dc6607c
Juan Diaz <juanbacanp3s@gmail.com>
2024-04-05 10:49:28 -0300

    uniendo los css en uno solo unicamente para lo que es el index, cambiandole el nombre al css del index, cambiandole el hover a la barra de navegacion, cambiandole el alert de la consulta del index usando sweetalert y agregando un ico a la parte de la pestaña

--- js/adminInicio.js
+++ js/adminInicio.js
@@ -1,13 +1,21 @@
 const formConsulta = document.getElementById('formConsulta');
 
 formConsulta.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Aquí puedes agregar el código para enviar la consulta si es necesario
-    
-    // Limpiar el formulario después de enviarlo
-    formConsulta.reset();
-
-    // Mostrar un mensaje de alerta
-    alert('¡Su consulta se envió correctamente!');
+    event.preventDefault();    
+// LIMPIA CONSULTA UNA VEZ ENVIADA
+formConsulta.reset();
+// MOSTRANDO EL MENSAJE DE ALERTA CON SWEETALERT
+Swal.fire({
+    title: "Listo!",
+    text: "Su consulta se envió correctamente, en breve nos comunicaremos con usted.",
+    imageUrl: "/img/success.png",
+    imageWidth: 212,
+    imageHeight: 212,
+    imageAlt: "Imagen de envio de consulta confirmada"
+  });
+// Swal.fire({
+//     icon: "success",
+//     title: "Consulta enviada",
+//     text: "Su consulta fue enviada con exito, en breve nos comunicaremos con usted.",
+//   });
 });
